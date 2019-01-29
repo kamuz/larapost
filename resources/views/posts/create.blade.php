a87a0fcc8c978ce01232542a559651c5025000b0
Vladimir Kamuz <v.kamuz@gmail.com>
2019-01-29 22:53:15 +0200

    Create and messages views

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Create Post</h1>
+    <form action="{{route('posts.store')}}" method="POST">
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" class="form-control" placeholder="Title">
+        </div>
+        <div class="form-group">
+            <label for="body">Body</label>
+            <textarea name="body" id="body" class="form-control" placeholder="Body" cols="30" rows="10"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Submit</button>
+        {{csrf_field()}}
+        <a href="/posts" class="btn btn-danger"><i class="fa fa-remove"></i> Cancel</a>
+    </form>
+@endsection
