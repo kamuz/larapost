e831f951190fc0132849aa04cda81080e55c5735
Vladimir Kamuz <v.kamuz@gmail.com>
2021-11-01 00:13:14 +0200

    Updated dump SQL

--- laracms.sql
+++ laracms.sql
@@ -1,11 +1,10 @@
--- Adminer 4.6.2 MySQL dump
+-- Adminer 4.7.8 MySQL dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP DATABASE IF EXISTS `laracms`;
 CREATE DATABASE `laracms` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `laracms`;
 
@@ -19,7 +18,6 @@ CREATE TABLE `migrations` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-TRUNCATE `migrations`;
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1,	'2014_10_12_000000_create_users_table',	1),
 (2,	'2014_10_12_100000_create_password_resets_table',	1),
@@ -35,7 +33,6 @@ CREATE TABLE `password_resets` (
   KEY `password_resets_email_index` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-TRUNCATE `password_resets`;
 
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
@@ -49,13 +46,12 @@ CREATE TABLE `posts` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-TRUNCATE `posts`;
 INSERT INTO `posts` (`id`, `created_at`, `updated_at`, `title`, `body`, `user_id`, `image`) VALUES
-(1,	'2019-01-25 21:05:26',	'2019-02-01 08:35:08',	'First Post',	'This is the post body',	1,	'12_1549017308.jpg'),
-(2,	'2019-01-25 21:05:48',	'2019-02-01 08:28:12',	'Second Post',	'Another body for second post',	1,	'10_1549016892.jpg'),
-(3,	'2019-01-29 16:38:17',	'2019-02-01 08:26:53',	'Third Post',	'Hello, world!',	1,	'08_1549016813.jpg'),
-(4,	'2019-01-29 17:25:21',	'2019-02-01 08:28:27',	'Four Post',	'Here is the awesome content',	1,	'09_1549016907.jpg'),
-(6,	'2019-01-29 21:33:08',	'2019-02-01 08:34:32',	'Fifth Post',	'Cool....',	2,	'11_1549017272.jpg');
+(1,	'2019-01-25 21:05:26',	'2021-10-31 19:58:43',	'First Post',	'This is the post body',	1,	'Desert_1635717523.jpg'),
+(2,	'2019-01-25 21:05:48',	'2021-10-31 19:59:17',	'Second Post',	'Another body for second post',	1,	'Tulips_1635717557.jpg'),
+(3,	'2019-01-29 16:38:17',	'2021-10-31 19:59:08',	'Third Post',	'Hello, world!',	1,	'Hydrangeas_1635717548.jpg'),
+(4,	'2019-01-29 17:25:21',	'2021-10-31 19:51:55',	'Four Post',	'Here is the awesome content',	1,	'Chrysanthemum_1635717115.png'),
+(6,	'2019-01-29 21:33:08',	'2021-10-31 19:59:55',	'Fifth Post',	'Cool....',	2,	'Penguins_1635717595.jpg');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -71,9 +67,8 @@ CREATE TABLE `users` (
   UNIQUE KEY `users_email_unique` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-TRUNCATE `users`;
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1,	'Vladimir Kamuz',	'v.kamuz@gmail.com',	NULL,	'$2y$10$IJUEejCBqd5Gi3lyCfW38enuyiNzlujvqAcMNlMOWLsgULNpu4uvW',	'TTJ6uu8ErhwaKv9eInYm3AIXXRzbTCSZ6LGx1prMtfPGuzDtUoYsnq4egW0x',	'2019-01-29 20:40:22',	'2019-01-29 20:40:22'),
-(2,	'Alex Kamuz',	'a.kamuz@gmail.com',	NULL,	'$2y$10$FWdS4J0yKK2GiixAnIXvDuM1yGNS1F2RzQqlt6yUGIsEeh7jkbfXy',	'TjbJjAXuj1FAA5tJtGxc1t3roR7isaPYQCxny5Fg2uhfC1WUom0imnguao4F',	'2019-01-29 20:58:14',	'2019-01-29 21:03:51');
+(1,	'Vladimir Kamuz',	'v.kamuz@gmail.com',	NULL,	'$2y$10$IJUEejCBqd5Gi3lyCfW38enuyiNzlujvqAcMNlMOWLsgULNpu4uvW',	'05FqEwaOLbDNYhKDJ9rnVmopytlJA93gz40INBo3fyUerZFSHP8KJy01caBj',	'2019-01-29 20:40:22',	'2019-01-29 20:40:22'),
+(2,	'Alex Kamuz',	'a.kamuz@gmail.com',	NULL,	'$2y$10$FWdS4J0yKK2GiixAnIXvDuM1yGNS1F2RzQqlt6yUGIsEeh7jkbfXy',	'ctzwHzJMZi6G130H6heJZM4Q4dM2ZPlXF7SIjoSpSreDrtJQolMESJNJ60bD',	'2019-01-29 20:58:14',	'2019-01-29 21:03:51');
 
--- 2020-09-06 18:31:44
+-- 2021-10-31 22:12:29
