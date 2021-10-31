951bd25df758994a2d271af889bf5a04d2258b2b
Vladimir Kamuz <v.kamuz@gmail.com>
2021-11-01 00:06:29 +0200

    Replaced Font Awesome

--- public/js/app.js
+++ public/js/app.js
@@ -48107,8 +48107,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Server\domains\laracms.loc\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Server\domains\laracms.loc\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! d:\Server\domains\laracms.loc\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! d:\Server\domains\laracms.loc\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="lang="{{ str_replace('_', '-', app()->getLocale()) }}"">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <title>{{ config('app.name', 'Laravel APP') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
