48a3c0c502b26e0722ab1add09991435a749158e
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-04-19 12:51:38 +0300

    Fixed layout with button and footer

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,7 +15,7 @@
     @if(!Auth::guest())
         @if(Auth::user()->id == $post->user_id)
             <a href="/posts/{{$post->id}}/edit" class="btn btn-secondary"><i class="fa fa-pencil"></i> Edit Post</a>
-            <form action="{{route('posts.destroy', $post->id)}}" method="POST" class="pull-right mb-3">
+            <form action="{{route('posts.destroy', $post->id)}}" method="POST" class="pull-right">
                 <input type="hidden" name="_method" value="DELETE">
                 <button type="submit" class="btn btn-danger"><i class="fa fa-remove"></i> Delete Post</button>
                 {{csrf_field()}}
