96e291ddafd01dc599dc0f0109550b8848ac20d6
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-04-20 12:13:38 +0300

    Update config

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://larapost.loc'),
+    'url' => env('APP_URL', ''),
 
-    'asset_url' => env('ASSET_URL', 'https://larapost.loc/public'),
+    'asset_url' => env('ASSET_URL', ''),
 
     /*
     |--------------------------------------------------------------------------
